305a75ff96f4431fae42a44ceda678247a9b68b0
Chris A <cachuzia@kobobasket.com>
2024-12-14 16:11:48 -0500

    commit adding store

--- src/component/MenuContainer.tsx
+++ src/component/MenuContainer.tsx
@@ -9,7 +9,7 @@ export default function MenuContainer({ contentViewArea = '45px' }: { contentVie
 			<ScrollableContainer orientation="horizontal">
 				<StyledStack direction={'row'} gap={2} flexGrow={1}>
 					{
-						menus.map((menu, index) => <MenuButton key={index}>{menu}</MenuButton>)
+						menus.map((menu, index) => <MenuButton $isDeals={menu === 'DEALS'} key={index}>{menu}</MenuButton>)
 					}
 				</StyledStack>
 			</ScrollableContainer>
@@ -27,10 +27,12 @@ const StyledStack = styled(Stack)(({ theme }) => ({
 	}
 }));
 
-const MenuButton = styled(Button)(({ theme })=> ({
+const MenuButton = styled(Button, {
+	shouldForwardProp: prop => prop !== '$isDeals'
+})<{ $isDeals: boolean }>(({ theme, $isDeals })=> ({
 	minWidth: '90px',
 	fontFamily: 'Alata',
 	fontSize: '24px',
 	lineHeight: '133.4%',
-	color: theme.palette.primaryBlack.main
+	color: $isDeals ? theme.palette.primaryOrange.main : theme.palette.primaryBlack.main
 }));
